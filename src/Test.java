5bfa15ec4246afa8db91828e43411d00e464458e
lucky-tin <v.v.openchenko@student.khai.edu>
2024-11-20 13:48:25 +0200

    added a comment to Test.java on the feature branch

--- src/Test.java
+++ src/Test.java
@@ -1,7 +1,7 @@
 public class Test {
     public static void main(String[] args) {
         System.out.println("education");
-        //comment
+        //commen
         System.out.println("DICT");
     }
 }
