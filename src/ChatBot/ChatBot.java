3b025af9f9fbe74443e1dd5c1159f570bf289016
lucky-tin <v.v.openchenko@student.khai.edu>
2024-12-04 12:33:41 +0200

    Chatbot stage 2

--- src/ChatBot/ChatBot.java
+++ src/ChatBot/ChatBot.java
@@ -6,3 +6,17 @@ public class ChatBot {
     public static void main(String[] args) {
         // Визначте ім'я бота
         String botName = "Shadow Fiend";
+
+        // Визначте поточний рік
+        int birthYear = 2023;
+
+        // Створіть сканер для введення з клавіатури
+        Scanner scanner = new Scanner(System.in);
+
+        // Запитайте користувача про його ім'я
+        System.out.println("Hello! My name is " + botName + ".");
+        System.out.println("I was created in " + birthYear + ".");
+        System.out.println("Please, remind me your name.");
+
+        // Отримайте ім'я користувача з клавіатури
+        String yourName = scanner.nextLine();
