1bd11edbd1afc514c51d498e4a74c37daf65d7fb
lucky-tin <v.v.openchenko@student.khai.edu>
2024-12-04 12:32:02 +0200

    Chatbot stage 1

---
+++ src/ChatBot/ChatBot.java
@@ -0,0 +1,8 @@
+package ChatBot;
+
+import java.util.Scanner;
+public class ChatBot {
+
+    public static void main(String[] args) {
+        // Визначте ім'я бота
+        String botName = "Shadow Fiend";
