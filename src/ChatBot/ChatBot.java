c57857c49ead3a618648e5afbf46e3a61c6163ce
lucky-tin <v.v.openchenko@student.khai.edu>
2024-12-04 12:35:14 +0200

    Chatbot stage 5

--- src/ChatBot/ChatBot.java
+++ src/ChatBot/ChatBot.java
@@ -8,7 +8,7 @@ public class ChatBot {
         String botName = "Shadow Fiend";
 
         // Визначте поточний рік
-        int birthYear = 2024;
+        int birthYear = 2023;
 
         // Створіть сканер для введення з клавіатури
         Scanner scanner = new Scanner(System.in);
@@ -53,3 +53,55 @@ public class ChatBot {
         while (i <= userInp) {
             // Виведіть поточне число
             System.out.println(i + "!");
+
+            // Збільште i на 1
+            i++;
+            // Створіть сканер для введення з клавіатури
+            scanner = new Scanner(System.in);
+
+            // Запитайте користувача про його ім'я
+            System.out.println("Hello! My name is Bard.");
+            System.out.println("I will now give you a programming test.");
+
+            // Створіть список запитань і відповідей
+            String[] questions = {
+                    "Який тип даних використовується для зберігання цілих чисел?",
+                    "Який тип даних використовується для зберігання дійсних чисел?",
+                    "Який тип даних використовується для зберігання рядків?",
+                    "Який оператор використовується для присвоєння значення змінній?",
+                    "Який оператор використовується для порівняння двох змінних?",
+                    "Який оператор використовується для арифметичних операцій?"
+            };
+            String[] answers = {
+                    "int",
+                    "double",
+                    "String",
+                    "=",
+                    "==",
+                    "+, -, *, /"
+            };
+
+            // Повторюйте тест, поки користувач не відповість правильно
+            int correctAnswers = 0;
+            while (correctAnswers < questions.length) {
+                // Запитайте користувача запитання
+                System.out.println(questions[correctAnswers]);
+
+                // Отримайте відповідь від користувача
+                String userAnswer = scanner.nextLine();
+
+                // Перевірте відповідь користувача
+                if (userAnswer.equals(answers[correctAnswers])) {
+                    // Відповідь правильна
+                    System.out.println("Відповідь правильна!");
+                    correctAnswers++;
+                } else {
+                    // Відповідь неправильна
+                    System.out.println("Відповідь неправильна.");
+                }
+            }
+            // Привітайте користувача з завершенням тесту
+            System.out.println("Вітаю! Ви пройшли тест!");
+        }
+    }
+}
